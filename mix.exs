1f8f9b43cc6fab467deb55ae665f837038864baf
Fernando Sanchez <fernando.ivan.sanchez@outlook.com>
2021-03-07 11:07:32 -0500

    Added httpoison & poison
    HTTPoison for HTTP requests
    Poison for structuring JSON

--- mix.exs
+++ mix.exs
@@ -43,7 +43,10 @@ defmodule Weather.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:envy, "~> 1.1.1"}
+      {:envy, "~> 1.1.1"},
+      {:httpoison, "~> 1.8"},
+      {:poison, "~> 3.1"}
+
     ]
   end
 
