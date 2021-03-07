37d35b385405767139dcca89e53eb2638b17c45d
Fernando Sanchez <fernando.ivan.sanchez@outlook.com>
2021-03-07 12:03:39 -0500

    Happy path test
    Very simply - make a call to weather api, hope it comes back a 200

--- test/weather_web/controllers/weather_controller_test.exs
+++ test/weather_web/controllers/weather_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule WeatherWeb.WeatherControllerTest do
   use WeatherWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/weather")
-    assert json_response(conn, 200) == []
+  test "GET / happy path", %{conn: conn} do
+    conn = get(conn, "/weather?latitude=33.7984&longitude=-84.3883")
+    json_response(conn, 200)
   end
 end
