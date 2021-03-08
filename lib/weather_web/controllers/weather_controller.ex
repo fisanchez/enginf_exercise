66f5490f8a9ee642465ae7021189789d3a8779d6
Fernando Sanchez <fernando.ivan.sanchez@outlook.com>
2021-03-08 14:26:26 -0500

    Adding tests for missing params
    Added tests to test for missing lat, long

--- lib/weather_web/controllers/weather_controller.ex
+++ lib/weather_web/controllers/weather_controller.ex
@@ -4,13 +4,14 @@ defmodule WeatherWeb.WeatherController do
   def index(conn, params) do
     if params["latitude"] == nil || params["longitude"] == nil do
       conn
+      |> put_resp_content_type("text/plain; charset=utf8")
       |> send_resp(400, "Missing latitude or longitude param")
+    else
+      latitude = params['latitude']
+      longitude = params['longitude']
+
+      weather = Weather.format_query(latitude, longitude) |> Weather.get()
+      render(conn, "index.json", %{data: weather})
     end
-
-    latitude = params['latitude']
-    longitude = params['longitude']
-
-    weather = Weather.format_query(latitude, longitude) |> Weather.get()
-    render(conn, "index.json", %{data: weather})
   end
 end
--- test/weather_web/controllers/weather_controller_test.exs
+++ test/weather_web/controllers/weather_controller_test.exs
@@ -5,4 +5,19 @@ defmodule WeatherWeb.WeatherControllerTest do
     conn = get(conn, "/weather?latitude=33.7984&longitude=-84.3883")
     json_response(conn, 200)
   end
+
+  test "GET / missing params", %{conn: conn} do
+    conn = get(conn, "/weather")
+    assert text_response(conn,400) == "Missing latitude or longitude param"
+  end
+
+  test "GET / missing latitude params", %{conn: conn} do
+    conn = get(conn, "/weather?longitude=-84.3883")
+    assert text_response(conn,400) == "Missing latitude or longitude param"
+  end
+
+  test "GET / missing longitude params", %{conn: conn} do
+    conn = get(conn, "/weather?latitude=33.7984")
+    assert text_response(conn,400) == "Missing latitude or longitude param"
+  end
 end
