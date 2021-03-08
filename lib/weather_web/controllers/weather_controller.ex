ec76e752050947fff6dab3a9c8f213025e5bd5a8
Fernando Sanchez <fernando.ivan.sanchez@outlook.com>
2021-03-08 14:11:24 -0500

    Adding error handing for /weather
    Will return html error message when required params are nil

--- lib/weather_web/controllers/weather_controller.ex
+++ lib/weather_web/controllers/weather_controller.ex
@@ -1,7 +1,15 @@
 defmodule WeatherWeb.WeatherController do
   use WeatherWeb, :controller
 
-  def index(conn, %{"latitude" => latitude, "longitude" => longitude}) do
+  def index(conn, params) do
+    if params["latitude"] == nil || params["longitude"] == nil do
+      conn
+      |> send_resp(400, "Missing latitude or longitude param")
+    end
+
+    latitude = params['latitude']
+    longitude = params['longitude']
+
     weather = Weather.format_query(latitude, longitude) |> Weather.get()
     render(conn, "index.json", %{data: weather})
   end
